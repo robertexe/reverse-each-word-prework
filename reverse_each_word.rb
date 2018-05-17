92412618ff75ef8007248843c27804eac820f3be
Robert Tabares <robtabares92@gmail.com>
2018-05-17 17:27:15 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,7 +9,7 @@ end
 
 def reverse_each_word(sentence)
   sentence_array = sentence.split(' ')
-  sentence_array.collect! do |word|
+  sentence_array.collect do |word|
     word.reverse!
   end
   sentence_array.join(' ')
