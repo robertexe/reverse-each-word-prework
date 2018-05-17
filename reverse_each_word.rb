b6fbaeb24e088d1e7e19380e9cb0f7edc2eb280f
Robert Tabares <robtabares92@gmail.com>
2018-05-17 17:22:10 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(' ')
+  reversed_sentence = []
+  sentence_array.each do |word|
+    reversed_sentence << word.reverse!
+  end
+  reversed_sentence = reversed_sentence.join(' ')
+end
+
+def reverse_each_word(sentence)
+  sentence_array = sentence.split(' ')
+  sentence_array.collect do |word|
+    word.reverse!
+  end
+end
